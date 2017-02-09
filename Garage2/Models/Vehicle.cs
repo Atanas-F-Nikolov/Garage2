23378e8f49996c855981e1194dc420b636774194
Atanas F. Nikolov <a.f.nikolov@gmail.com>
2017-02-09 17:02:21 +0100

    Add validation to the user input when Check-in a vehicle.

--- Garage2/Models/Vehicle.cs
+++ Garage2/Models/Vehicle.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -17,13 +18,26 @@ namespace Garage2.Models
         public VehicleType Type { get; set; }
         
         [DisplayName("Registration number")]
+        [Required(ErrorMessage = "Please enter a registration number!")]
+        [MaxLength(10)]
+        [MinLength(4)]
+        [RegularExpression(@"^([a-zA-Z0-9 \-]+)$", ErrorMessage = "Please don't enter any special symbols!")]
         public string RegNumber { get; set; }
-        
+
+        [StringLength(10)]
+        [RegularExpression(@"^([a-zA-Z]+)$", ErrorMessage= "Please, enter only letters (a-z, A-Z)!")]
         public string Color { get; set; }
+
+        [StringLength(16)]
+        [RegularExpression(@"^([a-zA-Z \&\'\-]+)$", ErrorMessage = "Please enter only letters, '&', or \" !")]
         public string Brand { get; set; }
+
+        [StringLength(16)]
+        [RegularExpression(@"^([a-zA-Z0-9 \&\-]+)$", ErrorMessage = "Please enter letters, numbers, '&', or \" !")]
         public string Model { get; set; }
 
         [DisplayName("Number of wheels")]
+        [Range(0,16)]
         public int Wheels { get; set; }
 
         [DisplayName("Check-in time")]
